bb76ef58b3354e1096f647c6658842df33fab166
clutxh11 <95189058+clutxh11@users.noreply.github.com>
2022-05-26 18:43:20 -0400

    Made enties show correct data as in the figma.

--- src/pages/app/components/multiList/homeEntryRenderer.tsx
+++ src/pages/app/components/multiList/homeEntryRenderer.tsx
@@ -7,6 +7,30 @@ import { Ticket } from "../../../../services/types";
 import { fetchBroker } from "../../../../services/brokerServices";
 import useLoadable from "../../../../utils/useLoadable";
 
+function pickEntrySubtitle (status: Ticket["STATUS"]) {
+  switch(status){
+    case "DELIVERED":
+      return `Completed by: ${" "}`
+      break;
+
+    case "INCOMPLETE":
+      return `Reported by: ${" "}`
+      break;
+
+    case "IN-PROGRESS":
+      return `Assigned to: ${" "}`
+      break;
+
+    case "ASSIGNED":
+      return `Reported by: ${" "}`
+      break;
+
+    case "INVENTORY":
+      return `Unassigned`
+      break;
+  }
+}
+
 export default function EntryRenderer(entry: Ticket): JSX.Element {
   const {
     val: broker,
@@ -17,12 +41,14 @@ export default function EntryRenderer(entry: Ticket): JSX.Element {
     <Paper className="ss-ticket-renderer">
       <Typography>{entry.CONSIGNEE.ADDRESS}</Typography>
       <Typography>
-        Assigned to:{" "}
-        {loading
+        {pickEntrySubtitle(entry.STATUS)}
+        {entry.STATUS === "INVENTORY"
+          ? ""
+          : loading
           ? "loading..."
           : broker
           ? broker.NAME
-          : error || "Error fetching broker"}
+          : error || "Error fetching broker" }
       </Typography>
     </Paper>
   );
