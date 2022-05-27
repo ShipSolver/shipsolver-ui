4b5d9e8fa4af2db75ab3a6e2831041c8c68ab437
Derek Chen <d329chen@uwaterloo.ca>
2022-05-27 14:59:41 -0400

    added row selection

--- src/pages/components/allTicketsTable/components/ticketTable.tsx
+++ src/pages/components/allTicketsTable/components/ticketTable.tsx
@@ -7,6 +7,7 @@ import TableCell from "@mui/material/TableCell";
 import Paper from "@mui/material/Paper";
 import TableBody from "@mui/material/TableBody";
 import { DropdownButton } from "./dropdownButton";
+import Checkbox from "@mui/material/Checkbox";
 
 interface HeaderRowDataType {
   label: string;
@@ -31,6 +32,36 @@ export const TicketTable = <T extends string>({
   headerRow,
   rows,
 }: TicketTableProps<T>) => {
+  const [selected, setSelected] = React.useState<{ [key: string]: boolean }>(
+    Object.values(rows).reduce(
+      (selected, row) => ({ ...selected, [row.ticketID]: false }),
+      {}
+    )
+  );
+
+  const [allSelected, setAllSelected] = React.useState<boolean>(false);
+
+  console.log(selected);
+  console.log(rows);
+
+  const handleSingleClick = (ticketID: string) => {
+    console.log(ticketID, typeof ticketID);
+    setSelected((prev) => ({
+      ...prev,
+      [ticketID]: !prev[ticketID],
+    }));
+  };
+
+  const handleSelectAllClick = (rows: RowType<T>[]) => {
+    setAllSelected(!allSelected);
+    setSelected(
+      Object.values(rows).reduce(
+        (selected, row) => ({ ...selected, [row.ticketID]: !allSelected }),
+        {}
+      )
+    );
+  };
+
   const headerRowData: HeaderRowDataType[] = Object.values(headerRow);
   const headerFilters = headerRowData.map(
     ({ filterLabel, filterContent }, i) => {
@@ -45,27 +76,49 @@ export const TicketTable = <T extends string>({
     }
   );
   const headerLabels = headerRowData.map(({ label }) => (
-    <TableCell align="left">{label as string}</TableCell>
+    <TableCell align="left" sx={{ fontWeight: "bold" }}>
+      {label as string}{" "}
+    </TableCell>
   ));
 
   const tableRows = useMemo(
     () =>
-      rows.map((row, i) => (
-        <TableRow key={i}>
-          {Object.values(row).map((val) => (
-            <TableCell align="left">{val as any}</TableCell>
-          ))}
-        </TableRow>
-      )),
-    [rows]
+      rows.map((row, i) => {
+        return (
+          <TableRow key={i} hover selected={selected[row.ticketID]}>
+            <TableCell padding="checkbox">
+              <Checkbox
+                checked={selected[row.ticketID]}
+                onClick={(event) => handleSingleClick(row.ticketID)}
+              />
+            </TableCell>
+            {Object.entries(row).map(([key, val]) => {
+              if (key === "ticketID") return null;
+              return <TableCell align="left">{val as any}</TableCell>;
+            })}
+          </TableRow>
+        );
+      }),
+    [rows, selected]
   );
 
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
-          <TableRow>{headerFilters}</TableRow>
-          <TableRow>{headerLabels}</TableRow>
+          <TableRow>
+            <TableCell></TableCell>
+            {headerFilters}
+          </TableRow>
+          <TableRow>
+            <TableCell padding="checkbox">
+              <Checkbox
+                checked={allSelected}
+                onClick={(event) => handleSelectAllClick(rows)}
+              />
+            </TableCell>
+            {headerLabels}
+          </TableRow>
         </TableHead>
         <TableBody>{tableRows}</TableBody>
       </Table>
