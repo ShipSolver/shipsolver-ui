0a117cb022fc251e3afbb9a236e74cc046378f38
SeanN30 <narandeniyas30@gmail.com>
2022-06-10 13:45:00 -0400

    ticketFactory update

--- src/pages/app/ticketFactory.tsx
+++ src/pages/app/ticketFactory.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
 import { sizing } from '@mui/system';
 import Paper from "../components/roundedPaper";
 import { blue } from "@mui/material/colors";
@@ -28,6 +28,7 @@ function Stuff(){
 
   function handleChange(event:any) {
     console.log(event.target.value)
+    console.log(event.target.name)
     setFormData(prevFormData => {
         return {
             ...prevFormData,
@@ -60,111 +61,106 @@ function Stuff(){
   })
   }
 
-  function Column1(){
-    return(
-      <div>
-        <form onSubmit={handleSubmit}>
-      <div className = "column1">
-        <div>
-          <h1 className="unbold"> Shipper </h1>
-            <div className="Ticketlabel">
-              <p className = "label-header"> Company </p>
-              <input className = "label-textbox" value={formData.company} onChange={handleChange} type="text" name = "company" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-
-            <div className="Ticketlabel">
-                <p className = "label-header"> Name </p>
-                <input value={formData.Name} onChange={handleChange} className = "label-textbox" type="text" name = "Name" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-
-            <div className="Ticketlabel">
-              <p className = "label-header"> Address </p>
-              <input className = "label-textbox" value={formData.Address} onChange={handleChange} type="text" name = "Address" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-
-            <div className="Ticketlabel">
-              <p className = "label-header"> Phone Number </p>
-              <input className = "label-textbox" value={formData.PhoneNumber} onChange={handleChange} type="text" name = "PhoneNumber" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-
-            <div className="Ticketlabel">
-              <p className = "label-header"> Postal Code </p>
-              <input className = "label-textbox" value={formData.PostalCode} onChange={handleChange} type="text" name = "PostalCode" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-          </div>
-          <div>
-          <h1 className="unbold"> Consignee </h1>
-          <div className="Ticketlabel">
-              <p className = "label-header"> Company </p>
-              <input className = "label-textbox" value={formData.Company1} onChange={handleChange} type="text" name = "Company1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-
-            <div className="Ticketlabel">
-                <p className = "label-header"> Name </p>
-                <input value={formData.Name1} onChange={handleChange} className = "label-textbox" type="text" name = "Name1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-
-            <div className="Ticketlabel">
-              <p className = "label-header"> Address </p>
-              <input className = "label-textbox" value={formData.Address1} onChange={handleChange} type="text" name = "Address1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-
-            <div className="Ticketlabel">
-              <p className = "label-header"> Phone Number </p>
-              <input className = "label-textbox" value={formData.PhoneNumber1} onChange={handleChange} type="text" name = "PhoneNumber1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-
-            <div className="Ticketlabel">
-              <p className = "label-header"> Postal Code </p>
-              <input className = "label-textbox" value={formData.PostalCode1} onChange={handleChange} type="text" name = "PostalCode1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-          </div>
+  return(
+    <div>
+    <form onSubmit={handleSubmit}>
+  <div className = "column1">
+    <div>
+      <h1 className="unbold"> Shipper </h1>
+        <div className="Ticketlabel">
+          <p className = "label-header"> Company </p>
+          <input className = "label-textbox" value={formData.company} onChange={handleChange} type="text" name = "company" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
         </div>
-        <div>
-        <h1 className="unbold"> Shipment Details </h1>
-            <div className="Ticketlabel">
-              <p className = "label-header"> House/Ref </p>
-              <input className = "label-textbox" value={formData.HouseRef} onChange={handleChange} type="text" name = "HouseRef" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
 
-            <div className="Ticketlabel">
-              <p className = "label-header"> Barcode </p>
-              <input className = "label-textbox" value={formData.Barcode} onChange={handleChange} type="text" name = "Barcode" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
+        <div className="Ticketlabel">
+            <p className = "label-header"> Name </p>
+            <input value={formData.Name} onChange={handleChange} className = "label-textbox" type="text" name = "Name" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
 
-            <div className="Ticketlabel">
-              <p className = "label-header"> # of pieces </p>
-              <input className = "label-textbox" value={formData.pieces} onChange={handleChange} type="text" name = "pieces" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
+        <div className="Ticketlabel">
+          <p className = "label-header"> Address </p>
+          <input className = "label-textbox" value={formData.Address} onChange={handleChange} type="text" name = "Address" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
 
-            <div className="Ticketlabel">
-              <p className = "label-header"> Weight  </p>
-              <input className = "label-textbox" value={formData.Weight} onChange={handleChange} type="text" name = "Weight" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
+        <div className="Ticketlabel">
+          <p className = "label-header"> Phone Number </p>
+          <input className = "label-textbox" value={formData.PhoneNumber} onChange={handleChange} type="text" name = "PhoneNumber" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
 
-            <div className="Ticketlabel">
-              <p className = "label-header"> BOL # </p>
-              <input className = "label-textbox" value={formData.BOL} onChange={handleChange} type="text" name = "BOL" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
-            </div>
-
-          <div className = "di"> 
-          <p>Special Instructions</p>  
-          <textarea className = "label-textbox" value={formData.Special} onChange={handleChange} style={{ width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        <div className="Ticketlabel">
+          <p className = "label-header"> Postal Code </p>
+          <input className = "label-textbox" value={formData.PostalCode} onChange={handleChange} type="text" name = "PostalCode" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
         </div>
       </div>
-        <div className = "buttons">
-          <Button onClick = {handleSubmit} variant="contained" sx = {{ height: 32, backgroundColor: "yellow", color: "black", fontSize: 24, mr: 2}}>Add to Inventory</Button>
-          <OneButton name = "Delete" color = "red"/>
-          <Button onClick = {clearForm} variant="contained" sx = {{ height: 32, backgroundColor: "indigo[500]", color: "black", fontSize: 24, mr: 2}}>Clear</Button>
+      <div>
+      <h1 className="unbold"> Consignee </h1>
+      <div className="Ticketlabel">
+          <p className = "label-header"> Company </p>
+          <input className = "label-textbox" value={formData.Company1} onChange={handleChange} type="text" name = "Company1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
         </div>
-      
-      </form>
+
+        <div className="Ticketlabel">
+            <p className = "label-header"> Name </p>
+            <input value={formData.Name1} onChange={handleChange} className = "label-textbox" type="text" name = "Name1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
+
+        <div className="Ticketlabel">
+          <p className = "label-header"> Address </p>
+          <input className = "label-textbox" value={formData.Address1} onChange={handleChange} type="text" name = "Address1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
+
+        <div className="Ticketlabel">
+          <p className = "label-header"> Phone Number </p>
+          <input className = "label-textbox" value={formData.PhoneNumber1} onChange={handleChange} type="text" name = "PhoneNumber1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
+
+        <div className="Ticketlabel">
+          <p className = "label-header"> Postal Code </p>
+          <input className = "label-textbox" value={formData.PostalCode1} onChange={handleChange} type="text" name = "PostalCode1" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
+      </div>
+    </div>
+    <div>
+    <h1 className="unbold"> Shipment Details </h1>
+        <div className="Ticketlabel">
+          <p className = "label-header"> House/Ref </p>
+          <input className = "label-textbox" value={formData.HouseRef} onChange={handleChange} type="text" name = "HouseRef" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
+
+        <div className="Ticketlabel">
+          <p className = "label-header"> Barcode </p>
+          <input className = "label-textbox" value={formData.Barcode} onChange={handleChange} type="text" name = "Barcode" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
+
+        <div className="Ticketlabel">
+          <p className = "label-header"> # of pieces </p>
+          <input className = "label-textbox" value={formData.pieces} onChange={handleChange} type="text" name = "pieces" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
+
+        <div className="Ticketlabel">
+          <p className = "label-header"> Weight  </p>
+          <input className = "label-textbox" value={formData.Weight} onChange={handleChange} type="text" name = "Weight" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
+
+        <div className="Ticketlabel">
+          <p className = "label-header"> BOL # </p>
+          <input className = "label-textbox" value={formData.BOL} onChange={handleChange} type="text" name = "BOL" style={{ height: 20, width: "70%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+        </div>
+
+      <div className = "di"> 
+      <p>Special Instructions</p>  
+      <textarea value={formData.Special} name = "Special" onChange={handleChange} style={{ width: "90%", borderColor: 'gray', borderWidth: 1, borderRadius: 10, fontSize: 15 }}/>
+    </div>
+  </div>
+    <div className = "buttons">
+      <Button onClick = {handleSubmit} variant="contained" sx = {{ height: 32, backgroundColor: "yellow", color: "black", fontSize: 24, mr: 2}}>Add to Inventory</Button>
+      <OneButton name = "Delete" color = "red"/>
+      <Button onClick = {clearForm} variant="contained" sx = {{ height: 32, backgroundColor: "indigo[500]", color: "black", fontSize: 24, mr: 2}}>Clear</Button>
+    </div>
+  
+  </form>
 
 </div>
-    )
-  }
-  return(
-    <Column1/>
   )
 }
 
