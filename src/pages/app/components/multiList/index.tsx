b438393826494bf55e55b7fed6fd7152cf3fae67
clutxh11 <95189058+clutxh11@users.noreply.github.com>
2022-06-10 10:27:33 -0400

    added {}

--- src/pages/app/components/multiList/index.tsx
+++ src/pages/app/components/multiList/index.tsx
@@ -59,8 +59,9 @@ function initializeSelectedEntries<T> (
 ) : AllSelectedItemsState {
   const IDs: AllSelectedItemsState = {}
   for(var i = 0; i < indexedListSpecifications.length; i++)
-    for (var i2 = 0; i2 < indexedListSpecifications[i].entries.length; i2++)
+    for (var i2 = 0; i2 < indexedListSpecifications[i].entries.length; i2++){
       IDs[indexedListSpecifications[i].entries[i].listID][indexedListSpecifications[i].entries[i2]?.ID] = false
+    }
   return IDs
 }
 
