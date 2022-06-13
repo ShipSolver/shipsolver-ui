058b0d49717244b3f08f8c34ab9ee593f9672146
Derek Chen <d329chen@uwaterloo.ca>
2022-06-13 16:05:58 -0400

    removed unecessary filler part

--- src/pages/components/ticketDetails/components/eventHistory.tsx
+++ src/pages/components/ticketDetails/components/eventHistory.tsx
@@ -45,66 +45,51 @@ export const EventHistory = ({ events }: EventHistoryProps) => {
   );
 
   return (
-    <div style={{ display: "flex" }}>
-      <div
-        style={{
-          backgroundColor: "#000",
-          width: "1150px",
-          height: "200px",
-          marginRight: 20,
-        }}
-      >
-        <Typography variant="h1" color="#FFF">
-          Hello
-        </Typography>
-      </div>
-
-      <div
-        className="event-history-container"
-        style={{
-          borderRadius: "var(--ss-brand-border-radius)",
-          backgroundColor: theme.palette.secondary.main,
-          flexGrow: 1,
-        }}
-      >
-        <Grid container direction="column" alignItems="center">
-          <Grid item>
-            <Paper
-              style={{
-                display: "inline-block",
-              }}
-              sx={{
-                padding: 2,
-                paddingLeft: 4,
-                paddingRight: 4,
-                marginTop: 2,
-                marginBottom: 0,
-              }}
-            >
-              <Typography variant="h2" fontWeight="bold">
-                Event History
-              </Typography>
-            </Paper>
-          </Grid>
+    <div
+      className="event-history-container"
+      style={{
+        borderRadius: "var(--ss-brand-border-radius)",
+        backgroundColor: theme.palette.secondary.main,
+        flexGrow: 1,
+      }}
+    >
+      <Grid container direction="column" alignItems="center">
+        <Grid item>
+          <Paper
+            style={{
+              display: "inline-block",
+            }}
+            sx={{
+              padding: 2,
+              paddingLeft: 4,
+              paddingRight: 4,
+              marginTop: 2,
+              marginBottom: 0,
+            }}
+          >
+            <Typography variant="h2" fontWeight="bold">
+              Event History
+            </Typography>
+          </Paper>
         </Grid>
+      </Grid>
 
-        <Paper
-          style={{
-            maxHeight: "80vh",
-            backgroundColor: theme.palette.secondary.light,
-            margin: 20,
-            padding: 10,
-            overflowY: "scroll",
-          }}
-          sx={{
-            "&::-webkit-scrollbar": {
-              display: "none",
-            },
-          }}
-        >
-          {eventHistoryCards}
-        </Paper>
-      </div>
+      <Paper
+        style={{
+          maxHeight: "80vh",
+          backgroundColor: theme.palette.secondary.light,
+          margin: 20,
+          padding: 10,
+          overflowY: "scroll",
+        }}
+        sx={{
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
+      >
+        {eventHistoryCards}
+      </Paper>
     </div>
   );
 };
