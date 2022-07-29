0c31c004bd84f4b80b742bd10561517521c8740e
clutxh11 <95189058+clutxh11@users.noreply.github.com>
2022-07-29 15:22:40 -0400

    in progress

---
+++ src/pages/app/workerHome/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+
+import OuterBlueDivBox from '../driver/components/outerBlueDivBox';
+import InnerBlueDivBox from '../driver/components/innerBlueDivBox';
+
+export const CheckInDeliveries = () => {
+    return (
+        <div>
+            <OuterBlueDivBox>
+
+            </OuterBlueDivBox>
+        </div>
+    );
+}
+
+function WorkerHome() {
+  return (
+    <div>
+        <Typography>Check in Deliveries</Typography>
+    </div>
+  )
+}
+
+export default WorkerHome
