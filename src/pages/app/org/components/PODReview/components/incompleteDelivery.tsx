123086be51fd26658d1dfcdebd87341b226edc7c
Derek Chen <d329chen@uwaterloo.ca>
2022-08-05 16:37:48 -0400

    POD review

---
+++ src/pages/app/org/components/PODReview/components/incompleteDelivery.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface IncompleteDeliveryProps {}
+
+export const IncompleteDelivery = (props: IncompleteDeliveryProps) => {
+  return (
+    <div>
+      <h3>Reason for Incomplete Delivery</h3>
+      <p>Reason</p>
+    </div>
+  );
+};
