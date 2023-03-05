8a6c995e8647f0e92970eeebe566887fc0a48458
Sathira Katugaha <SGKat@SathBook-Pro.local>
2023-03-05 15:56:38 -0500

    fix mutli assign ticket

--- src/apps/org/pages/header.tsx
+++ src/apps/org/pages/header.tsx
@@ -128,10 +128,7 @@ const Header = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar
-                  alt="Sathira Katugaha"
-                  src="/static/images/avatar/2.jpg"
-                />
+                <Avatar alt="Sathira Katugaha" />
               </IconButton>
             </Tooltip>
             <Menu
--- src/services/driverServices.ts
+++ src/services/driverServices.ts
@@ -56,20 +56,30 @@ export const assignToDriver = async (
 ) => {
   let error: string | null = null;
   try {
-    await Promise.all(
-      ticketIDs.map((ticketId) =>
-        axios.post("/api/milestones/AssignmentMilestones", {
-          withCredentials: false,
-          data: {
-            ticketId,
-            oldStatus: "checked_into_inventory",
-            newStatus: "assigned",
-            assignedByUserId: userID,
-            assignedToUserId: driverID,
-          },
-        })
-      )
-    );
+    await axios.post("/api/milestones/AssignmentMilestones", {
+      withCredentials: false,
+      data: ticketIDs.map((ticketId) => ({
+        ticketId,
+        oldStatus: "checked_into_inventory",
+        newStatus: "assigned",
+        assignedByUserId: userID,
+        assignedToUserId: driverID,
+      })),
+    });
+    // await Promise.all(
+    //   ticketIDs.map((ticketId) =>
+    //     axios.post("/api/milestones/AssignmentMilestones", {
+    //       withCredentials: false,
+    //       data: {
+    //         ticketId,
+    //         oldStatus: "checked_into_inventory",
+    //         newStatus: "assigned",
+    //         assignedByUserId: userID,
+    //         assignedToUserId: driverID,
+    //       },
+    //     })
+    //   )
+    // );
   } catch (e: any) {
     error = e?.toString?.() || `error assigned ticket to driver: ${driverID}`;
   }
