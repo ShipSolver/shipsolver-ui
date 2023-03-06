3fbb8e756843146ffc4f7914b9bfbe025b5bad0e
Sathira Katugaha <SGKat@SathBook-Pro.local>
2023-03-06 01:11:53 -0500

    idl

--- src/apps/org/pages/home/components/list/ticketMenu.tsx
+++ src/apps/org/pages/home/components/list/ticketMenu.tsx
@@ -87,8 +87,6 @@ export function TicketMenu({
         return (
           <>
             <GoToDriverButton selected={selected} numSelected={numSelected} />
-            <Divider sx={{ borderBottomWidth: 2 }} />
-            <ReviewPODButton selected={selected} numSelected={numSelected} />
           </>
         );
       }
